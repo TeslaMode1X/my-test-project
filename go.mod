35d6e9e9e4ea099aab0733385465c4c45a69bc66
Tesla <anuar.musljumov@gmail.com>
2024-06-28 05:09:21 +0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/TeslaMode1X/my-test-project
---
+++ test.go
@@ -0,0 +1,19 @@
+package tester
+
+import "strconv"
+
+var age int = 15
+var name string = "Not Given"
+
+func AddInts(a, b int) int {
+	return a + b
+}
+
+func SayHello(userName string) string {
+	name = userName
+	return "Hi," + name
+}
+
+func GetInfo() string {
+	return name + strconv.Itoa(age)
+}
